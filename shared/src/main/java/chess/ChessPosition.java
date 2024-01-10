a34ddb29b4ccc7daec5afed0f225a9860a265f6a
CraigRoundy <croundy11@gmail.com>
2024-01-10 15:22:43 -0700

    Chess Position Class implemented (not tested)

--- shared/src/main/java/chess/ChessPosition.java
+++ shared/src/main/java/chess/ChessPosition.java
@@ -6,9 +6,20 @@ package chess;
  * Note: You can add to this class, but you may not alter
  * signature of the existing methods.
  */
+
+// Class represents the chess square position
+// Includes row, column
+// Row is 1-8, column is 1-8
 public class ChessPosition {
+    int row;
+    int col;
 
     public ChessPosition(int row, int col) {
+        if (row < 1 || row > 8 || col < 1 || col > 8) {
+            throw new IllegalArgumentException("Invalid position");
+        }
+        this.row = row;
+        this.col = col;
     }
 
     /**
@@ -16,7 +27,7 @@ public class ChessPosition {
      * 1 codes for the bottom row
      */
     public int getRow() {
-        throw new RuntimeException("Not implemented");
+        return row;
     }
 
     /**
@@ -24,6 +35,6 @@ public class ChessPosition {
      * 1 codes for the left row
      */
     public int getColumn() {
-        throw new RuntimeException("Not implemented");
+        return col;
     }
 }
