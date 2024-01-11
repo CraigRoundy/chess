c45876fe045a6c8b1bb6a05a5cc9d391a1801c04
CraigRoundy <croundy11@gmail.com>
2024-01-10 17:44:05 -0700

    using vscode now on this branch.

--- shared/src/main/java/chess/ChessPosition.java
+++ shared/src/main/java/chess/ChessPosition.java
@@ -1,12 +1,5 @@
 package chess;
 
-/**
- * Represents a single square position on a chess board
- * <p>
- * Note: You can add to this class, but you may not alter
- * signature of the existing methods.
- */
-
 // Class represents the chess square position
 // Includes row, column
 // Row is 1-8, column is 1-8
@@ -22,18 +15,10 @@ public class ChessPosition {
         this.col = col;
     }
 
-    /**
-     * @return which row this position is in
-     * 1 codes for the bottom row
-     */
     public int getRow() {
         return row;
     }
 
-    /**
-     * @return which column this position is in
-     * 1 codes for the left row
-     */
     public int getColumn() {
         return col;
     }
