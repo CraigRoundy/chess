0041312959a9f67a5f78d00797bc097fd54f566c
Craig Roundy <croundy11@gmail.com>
2024-06-28 12:51:05 -0600

    Updated Chess Position file (All tests passed)

--- shared/src/main/java/chess/ChessPosition.java
+++ shared/src/main/java/chess/ChessPosition.java
@@ -7,8 +7,12 @@ package chess;
  * signature of the existing methods.
  */
 public class ChessPosition {
+    private int row;
+    private int col;
 
     public ChessPosition(int row, int col) {
+        setColumn(col);
+        setRow(row);
     }
 
     /**
@@ -16,7 +20,7 @@ public class ChessPosition {
      * 1 codes for the bottom row
      */
     public int getRow() {
-        throw new RuntimeException("Not implemented");
+        return row;
     }
 
     /**
@@ -24,6 +28,38 @@ public class ChessPosition {
      * 1 codes for the left row
      */
     public int getColumn() {
-        throw new RuntimeException("Not implemented");
+        return col;
+    }
+
+    //Set row variable given input is valid
+    public void setRow(int row) {
+        if (row < 1 || row > 8) {
+            throw new IllegalArgumentException("Invalid row number");
+        }
+        this.row = row;
+    }
+
+    //Set col variable given input is valid
+    public void setColumn(int col) {
+        if (col < 1 || col > 8) {
+            throw new IllegalArgumentException("Invalid column number");
+        }
+        this.col = col;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        ChessPosition that = (ChessPosition) o;
+        return row == that.row && col == that.col;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = 42;
+        result = 31 * result + row;
+        result = 31 * result + col;
+        return result;
     }
 }
